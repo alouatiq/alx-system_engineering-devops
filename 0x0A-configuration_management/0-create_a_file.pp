18bc1da6bab2324fb279b91ef81f511773f34f64
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-10 12:15:59 +0100

    Create 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This manifest creates a file in /tmp with specific attributes
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
