19ddb7d1535cfff451e2624aba7c21ef9cdaea02
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-13 22:33:56 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,39 @@
-class nginx {
-    package { 'nginx':
-        ensure => installed,
-    }
+# Puppet manifest to install and configure Nginx
 
-    service { 'nginx':
-        ensure => running,
-        enable => true,
-    }
-
-    file { '/var/www/html/index.html':
-        ensure  => file,
-        content => 'Hello World!',
-    }
-
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => template('nginx/default.erb'),
-        notify  => Service['nginx'],
-    }
+# Install Nginx
+package { 'nginx':
+  ensure => installed,
 }
 
-class { 'nginx': }
+# Create a custom index file
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => file,
+  content => "Hello World!",
+}
+
+# Configure Nginx to listen on port 80
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.nginx-debian.html;
+    server_name _;
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location = /404.html {
+        root /var/www/html;
+    }
+}
+",
+}
+
+# Restart Nginx
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
