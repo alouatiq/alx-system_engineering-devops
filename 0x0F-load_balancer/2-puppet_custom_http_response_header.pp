0a11213a6a016dfc548ed46be53cd033ab4dc2e2
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-20 22:07:31 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,11 +1,22 @@
-file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx_default_config.erb'),
-    notify  => Service['nginx'],
+class nginx_custom_header {
+    # Ensure Nginx is installed
+    package { 'nginx':
+        ensure => installed,
+    }
+
+    # Manage Nginx default site configuration
+    file { '/etc/nginx/sites-available/default':
+        ensure  => file,
+        content => template('nginx/default.erb'),
+        notify  => Service['nginx'],
+    }
+
+    # Manage Nginx service
+    service { 'nginx':
+        ensure     => running,
+        enable     => true,
+        hasrestart => true,
+    }
 }
 
-service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-}
+include nginx_custom_header
