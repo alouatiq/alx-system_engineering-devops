f7d5591925bef9ffc64be182237b7b0bef0622ae
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-20 22:10:26 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,57 +5,41 @@ package { 'nginx':
   ensure => installed,
 }
 
-# Modify the Nginx configuration to add the custom header
-file { '/etc/nginx/nginx.conf':
+# Modify the Nginx default configuration to add the custom header
+file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
+  content => epp('nginx/default.epp'),
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
 
-# Restart Nginx to apply the changes
+# Template for Nginx default configuration
+file { 'nginx/default.epp':
+  ensure  => file,
+  content => @(END),
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+            add_header X-Served-By <%= $::hostname %>;
+            try_files $uri $uri/ =404;
+        }
+    }
+    | END,
+  require => Package['nginx'],
+}
+
+# Ensure Nginx is running and enabled
 service { 'nginx':
   ensure    => running,
   enable    => true,
   require   => Package['nginx'],
-  subscribe => File['/etc/nginx/nginx.conf'],
-}
-
-# Template for Nginx configuration
-file { '/etc/nginx/nginx.conf.erb':
-  ensure  => file,
-  content => epp('nginx/nginx.conf.epp'),
-  require => Package['nginx'],
-}
-
-# Template content for Nginx configuration
-file { 'nginx/nginx.conf.epp':
-  ensure  => file,
-  content => @(END),
-    user www-data;
-    worker_processes auto;
-    pid /run/nginx.pid;
-    include /etc/nginx/modules-enabled/*.conf;
-
-    events {
-      worker_connections 768;
-    }
-
-    http {
-      include /etc/nginx/mime.types;
-      default_type application/octet-stream;
-
-      server {
-        listen 80;
-        server_name _;
-
-        location / {
-          add_header X-Served-By <%= $::hostname %>;
-          root /var/www/html;
-          index index.html index.htm;
-        }
-      }
-    }
-    | END,
-  require => Package['nginx'],
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
