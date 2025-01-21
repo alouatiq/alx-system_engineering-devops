5c67a32ce7f6b7eb64c0eb6f9e51d70bcd20477f
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-21 11:26:54 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,45 +1,28 @@
-# 2-puppet_custom_http_response_header.pp
-
-# Ensure Nginx is installed
 package { 'nginx':
   ensure => installed,
 }
 
-# Modify the Nginx default configuration to add the custom header
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => epp('nginx/default.epp'),
+  content => @("EOF"),
+    server {
+        listen 80;
+        server_name _;
+
+        location / {
+            add_header X-Served-By \$hostname;
+            root /var/www/html;
+            index index.html index.htm;
+        }
+    }
+    | EOF
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
 
-# Template for Nginx default configuration
-file { 'nginx/default.epp':
-  ensure  => file,
-  content => @(END),
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-            add_header X-Served-By <%= $::hostname %>;
-            try_files $uri $uri/ =404;
-        }
-    }
-    | END,
-  require => Package['nginx'],
-}
-
-# Ensure Nginx is running and enabled
 service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  require   => Package['nginx'],
-  subscribe => File['/etc/nginx/sites-available/default'],
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
 }
