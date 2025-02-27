90a1475b266fa10e286f7410172dfd65c0822b92
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-27 08:53:13 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,17 @@
-# Fixing WordPress .phpp => .php bug and ensuring Apache is running
-Exec { path => ['/bin','/usr/bin','/usr/sbin'] }
+# This Puppet manifest fixes the WordPress 500 error caused by a .phpp reference in wp-settings.php
+
+Exec { path => ['/bin', '/usr/bin', '/usr/sbin'] }
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  notify  => Service['apache2'],  # triggers Apache reload on change
+  # Use sed to replace .phpp with .php in wp-settings.php
+  command => '/bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+  # Only run if the .phpp typo is still present
+  onlyif  => 'grep -q "class-wp-locale.phpp" /var/www/html/wp-settings.php',
+  # Notify Apache so it can reload if the file changed
+  notify  => Service['apache2'],
 }
 
 service { 'apache2':
-  ensure => 'running',
+  ensure => running,
+  enable => true,
 }
