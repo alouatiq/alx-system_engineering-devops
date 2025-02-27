b887371e2289b531ca7e11cc53a434c0652176ec
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-27 08:51:49 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,43 @@
-# This Puppet manifest fixes the WordPress 500 error caused by a .phpp reference in wp-settings.php
+# This Puppet manifest fixes the WordPress 500 error by installing Apache/PHP, fixing .phpp typo, and enabling mod_rewrite
 
+# Ensures all Exec resources can find commands like a2enmod in /usr/sbin
 Exec { path => ['/bin', '/usr/bin', '/usr/sbin'] }
 
-exec { 'fix-wordpress':
-  # Use sed to replace .phpp with .php in wp-settings.php
+# 1) Make sure Apache is installed
+package { 'apache2':
+  ensure => installed,
+}
+
+# 2) Install PHP5 and MySQL extension for Ubuntu 14.04
+package { ['php5', 'php5-mysql']:
+  ensure  => installed,
+  require => Package['apache2'],
+}
+
+# 3) Ensure Apache service is running
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => [ Package['apache2'], Package['php5'], Package['php5-mysql'] ],
+}
+
+# 4) Fix the .phpp -> .php typo in wp-settings.php
+exec { 'fix-wordpress-typo':
   command => '/bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
-  # Only run if the .phpp typo is still present
   onlyif  => 'grep -q "class-wp-locale.phpp" /var/www/html/wp-settings.php',
-  # Notify Apache so it can reload if the file changed
   notify  => Service['apache2'],
 }
 
-service { 'apache2':
-  ensure => running,
-  enable => true,
+# 5) Set correct ownership & permissions for WordPress files
+exec { 'fix-wordpress-permissions':
+  command => 'chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html',
+  onlyif  => 'test -d /var/www/html',
+  require => Service["apache2"],
+}
+
+# 6) Enable mod_rewrite (common WordPress requirement)
+exec { 'enable-mod-rewrite':
+  command => 'a2enmod rewrite && service apache2 restart',
+  unless  => 'test -L /etc/apache2/mods-enabled/rewrite.load',
+  require => Service["apache2"],
 }
