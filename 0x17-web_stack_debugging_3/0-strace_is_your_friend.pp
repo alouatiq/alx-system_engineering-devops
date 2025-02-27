89146fb2ed4871c89b0838707767a902989ff741
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-27 08:44:48 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,24 +1,29 @@
 # This Puppet manifest fixes the WordPress 500 error by installing missing packages, setting permissions, etc.
+Exec { path => ['/bin', '/usr/bin', '/usr/sbin'] }
+
+package { 'apache2':
+  ensure => installed,
+}
+
 package { ['php5', 'php5-mysql']:
   ensure => installed,
+  require => Package['apache2'],
 }
 
 service { 'apache2':
   ensure  => running,
   enable  => true,
-  require => Package['php5'],
+  require => Package['apache2'],
 }
 
 exec { 'fix-wordpress-permissions':
   command => 'chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html',
-  path    => ['/bin', '/usr/bin'],
   onlyif  => 'test -d /var/www/html',
   require => Service['apache2'],
 }
 
 exec { 'enable-mod-rewrite':
   command => 'a2enmod rewrite && service apache2 restart',
-  path    => ['/bin', '/usr/bin'],
   unless  => 'test -L /etc/apache2/mods-enabled/rewrite.load',
   require => Service['apache2'],
 }
