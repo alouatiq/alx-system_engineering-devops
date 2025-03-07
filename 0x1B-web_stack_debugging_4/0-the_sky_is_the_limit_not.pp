3e59ae206ea9a5d9b170f4e186d7a8570975651d
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-03-07 06:41:14 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
 # 0-the_sky_is_the_limit_not.pp
-# This Puppet manifest configures Nginx & the OS to handle higher concurrency and eliminate failed requests
+# This Puppet manifest fixes Nginx so it can handle high concurrency and eliminate failed requests
 
-# 1. Ensure Nginx is installed and running
 package { 'nginx':
   ensure => installed,
 }
@@ -9,29 +8,11 @@ package { 'nginx':
 service { 'nginx':
   ensure     => running,
   enable     => true,
-  subscribe  => Package['nginx'],  # if package updates, restart service
+  require    => Package['nginx'],
 }
 
-# 2. Raise OS file descriptor limits (helps prevent "Too many open files")
-exec { 'increase_file_limits':
-  command => 'echo "* soft nofile 65536" >> /etc/security/limits.conf &&
-              echo "* hard nofile 65536" >> /etc/security/limits.conf &&
-              sed -i "s/^#\\?session required pam_limits.so/session required pam_limits.so/" /etc/pam.d/common-session &&
-              sed -i "s/^#\\?session required pam_limits.so/session required pam_limits.so/" /etc/pam.d/common-session-noninteractive',
-  unless  => 'grep "65536" /etc/security/limits.conf',
-}
-
-# 3. Increase worker_connections in Nginx config
-#    Use a generic regex so we replace any existing value
-exec { 'increase_nginx_worker_connections':
-  command => 'sed -i "s/worker_connections [0-9]\\+;/worker_connections 4096;/" /etc/nginx/nginx.conf',
-  unless  => 'grep "worker_connections 4096;" /etc/nginx/nginx.conf',
-  require => [ Package['nginx'], Exec['increase_file_limits'] ],
-}
-
-# 4. Restart Nginx only if config changed
-exec { 'restart_nginx':
-  command     => '/usr/sbin/service nginx restart',
-  refreshonly => true,
-  subscribe   => Exec['increase_nginx_worker_connections'],
+exec { 'fix_nginx_worker_connections':
+  command => 'sed -i "s/worker_connections [0-9]\\+;/worker_connections 1024;/" /etc/nginx/nginx.conf && service nginx restart',
+  unless  => 'grep "worker_connections 1024;" /etc/nginx/nginx.conf',
+  require => Service['nginx'],
 }
