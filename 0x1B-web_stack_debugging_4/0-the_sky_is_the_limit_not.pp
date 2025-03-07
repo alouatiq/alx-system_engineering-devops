899fae0cca3fe8145162663850776c44ded53926
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-03-07 06:34:19 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,9 @@
 # 0-the_sky_is_the_limit_not.pp
-# This Puppet manifest raises the Nginx worker connections to handle more requests concurrently
+# This Puppet manifest configures Nginx to handle higher concurrency with minimal failed requests
 
-exec { 'fix_nginx_worker_connections':
-  command => 'sed -i "s/worker_connections 768;/worker_connections 1024;/" /etc/nginx/nginx.conf && service nginx restart',
-  unless  => 'grep "worker_connections 1024;" /etc/nginx/nginx.conf',
+exec { 'fix_nginx_worker_config':
+  command => 'sed -i "s/worker_processes .*/worker_processes auto;/" /etc/nginx/nginx.conf && \
+              sed -i "s/worker_connections .*/worker_connections 4096;/" /etc/nginx/nginx.conf && \
+              service nginx restart',
+  unless  => 'grep -q "worker_processes auto;" /etc/nginx/nginx.conf && grep -q "worker_connections 4096;" /etc/nginx/nginx.conf',
 }
